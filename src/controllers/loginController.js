8c331b11ffdfecd58e741c5d4a016a5c4de51ce3
williamL3y35 <williamfernandoleyes@gmail.com>
2020-08-04 19:10:55 -0500

    listo para login2

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,5 +3,11 @@ const loginController = {};
 loginController.inicio = (req,res) => {
     res.render('login');
 }
+loginController.validar = (req,res) => {
+    req.getConnection((err,conn) => {
+        var correo =conn.query('SELECT usuario.usuario FROM usuario WHERE cc_usuario = 1');
+        console.log(correo);
+    })
+}
 
 module.exports = loginController;
--- src/routes/login.js
+++ src/routes/login.js
@@ -3,6 +3,7 @@ const routerLogin = express.Router();
 const loginController = require('../controllers/loginController');
 
 routerLogin.get('/', loginController.inicio);
+routerLogin.post('/validar', loginController.validar);
 
 
 module.exports = routerLogin;
