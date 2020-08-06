7e79d65486de02bf44c2477d28a0b50d3b6c7ac3
williamL3y35 <williamfernandoleyes@gmail.com>
2020-08-05 21:50:36 -0500

    listo login, falta sesiones

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,9 +4,20 @@ loginController.inicio = (req,res) => {
     res.render('login');
 }
 loginController.validar = (req,res) => {
+    var usuariotemp = req.body.usuario;
+    var contraseñatemp = req.body.contraseña_usuario;
     req.getConnection((err,conn) => {
-        var correo =conn.query('SELECT usuario.usuario FROM usuario WHERE cc_usuario = 1');
-        console.log(correo);
+        conn.query('SELECT * FROM usuario WHERE usuario.usuario = ? AND usuario.contraseña_usuario = ?', [usuariotemp, contraseñatemp] , (err, correos) =>{
+            if(correos.length>0){
+                res.redirect('/user');
+                console.log('usuario y contraseña coinciden');
+            }else{
+                res.redirect('/');
+                console.log('no coinciden');
+            }
+            
+        })
+        
     })
 }
 
